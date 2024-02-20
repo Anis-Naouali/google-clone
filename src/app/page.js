fa51fb998c32888170476bea2305d7f651dbb9e7
Anis-Naouali <anis.naouali@esprit.tn>
2024-02-20 02:57:59 +0100

    body

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,23 @@
-import React from 'react'
-import HomeHeader from './components/HomeHeader'
+import React from "react";
+import HomeHeader from "./components/HomeHeader";
+import Image from "next/image";
+import HomeSearch from "./components/HomeSearch";
 
 export default function Home() {
   return (
     <>
-    <HomeHeader/>
+      <HomeHeader />
+      <div className="flex flex-col items-center mt-24">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Google_logo_%282013-2015%29.svg/2560px-Google_logo_%282013-2015%29.svg.png"
+          alt="logo"
+          width={300}
+          height={100}
+          priority
+          style={{width:'auto'}}
+        />
+        <HomeSearch />
+      </div>
     </>
-  )
+  );
 }
