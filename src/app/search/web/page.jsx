754c037ee552b03121acb3850cb87e0fae2a9974
Anis-Naouali <anis.naouali@esprit.tn>
2024-02-22 00:59:35 +0100

    Pagination

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -3,8 +3,10 @@ import Link from "next/link";
 import React from "react";
 
 export default async function WebSearchPage({ searchParams }) {
+  const startIndex = searchParams.start || "1";
+
   const response = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}'}&start=${startIndex}`
   );
   if (!response.ok) throw new Error("Something went wrong");
   const data = await response.json();
@@ -23,9 +25,5 @@ export default async function WebSearchPage({ searchParams }) {
     );
   }
 
-  return (
-    <div>
-      {results && <WebSearchResults results={data} />}
-    </div>
-  );
+  return <div>{results && <WebSearchResults results={data} />}</div>;
 }
